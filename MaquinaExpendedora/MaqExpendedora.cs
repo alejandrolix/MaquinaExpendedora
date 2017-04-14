6729802fd3a924cdc02c395535703b914364fa70
Alejandro <alejandro.pons98@gmail.com>
2017-04-14 19:58:29 +0200

    Clases y Métodos Creados

---
+++ MaquinaExpendedora/MaqExpendedora.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaExpendedora
+{
+    class MaqExpendedora
+    {
+        #region Atributos y Propiedades
+
+        private String _Marca;
+        private String _Modelo;
+        private String _TelefonoAverias;
+        private List<Producto> _Productos;
+
+        public String Marca
+        {
+            get { return this._Marca; }
+            set { this._Marca = value; }
+        }
+        public String Modelo
+        {
+            get { return this._Modelo; }
+            set { this._Modelo = value; }
+        }
+        public String TelefonoAverias
+        {
+            get { return this._TelefonoAverias; }
+            set { this._TelefonoAverias = value; }
+        }
+        public List<Producto> Productos
+        {
+            get { return this._Productos; }
+            set { this._Productos = value; }
+        }
+
+        #endregion
+
+
+        #region Métodos
+
+        public void CambiarDatosMaquina()
+        {
+
+        }
+
+        public void SacarProducto()
+        {
+
+        }
+
+        public void AniadirProducto()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Constructores
+
+        public MaqExpendedora(String marca, String modelo, String telefonoAverias, List<Producto> productos)
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.TelefonoAverias = telefonoAverias;
+            this.Productos = new List<Producto>();
+        }
+
+        #endregion
+    }
+}
---
+++ MaquinaExpendedora/Producto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaExpendedora
+{
+    class Producto
+    {
+        #region Atributos y Propiedades
+
+        private static int _ID;
+        private String _Nombre;
+        private String _Marca;
+        private int _Cantidad;
+        private float _Precio;
+
+        public static int ID
+        {
+            get { return Producto._ID; }
+            set { Producto._ID = value; }
+        }
+        public String Nombre
+        {
+            get { return this._Nombre; }
+            set { this._Nombre = value; }
+        }
+        public String Marca
+        {
+            get { return this._Marca; }
+            set { this._Marca = value; }
+        }
+        public int Cantidad
+        {
+            get { return this._Cantidad; }
+            set { this._Cantidad = value; }
+        }
+        public float Precio
+        {
+            get { return this._Precio; }
+            set { this._Precio = value; }
+        }
+
+        #endregion
+
+
+        #region Métodos
+
+        public static int ObtenerSiguienteID()
+        {
+            return Producto.ID + 1;
+        }
+
+        public void CambiarDatosProducto(Producto producto)
+        {
+
+        }
+
+        #endregion
+
+
+
+        #region Constructor
+
+        public Producto(String nombre, String marca, int cantidad, float precio)
+        {
+            Producto.ID = ObtenerSiguienteID();
+            this.Nombre = nombre;
+            this.Marca = marca;
+            this.Cantidad = cantidad;
+            this.Precio = precio;
+        }
+
+        #endregion
+    }
+}
